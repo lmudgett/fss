2579603ecedd8ad30df36d820e7b4abaa1ae648d
Len Mudgett <lmudgett@gmail.com>
2024-06-03 08:10:29 -0400

    init

---
+++ src/main/java/org/mlp/fss/controllers/MainController.java
@@ -0,0 +1,17 @@
+package org.mlp.fss.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	
+	
+	
+}
